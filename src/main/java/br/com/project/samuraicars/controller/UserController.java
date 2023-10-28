e338afd7ce9c2f64ab22a43f94650fffe548b519
wesley <wesleybruno910@gmail.com>
2023-10-28 16:34:40 -0300

    fix: remove non-used imports

--- src/main/java/br/com/project/samuraicars/controller/UserController.java
+++ src/main/java/br/com/project/samuraicars/controller/UserController.java
@@ -6,7 +6,6 @@ import br.com.project.samuraicars.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
