78f9c3c8d77b5ff9d9f1190775717321d117ca7e
wesley <wesleybruno910@gmail.com>
2023-10-28 16:24:26 -0300

    fix: change the generic type of the entity
    the generic type of VehiclePhotoRepository was fixed to VehiclePhoto before it was VehiclePhotoRepository

--- src/main/java/br/com/project/samuraicars/repositoy/VehiclePhotoRepository.java
+++ src/main/java/br/com/project/samuraicars/repositoy/VehiclePhotoRepository.java
@@ -1,6 +1,7 @@
 package br.com.project.samuraicars.repositoy;
 
+import br.com.project.samuraicars.model.VehichlePhoto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VehiclePhotoRepository extends JpaRepository<VehicleRespository, Long> {
+public interface VehiclePhotoRepository extends JpaRepository<VehichlePhoto, Long> {
 }
