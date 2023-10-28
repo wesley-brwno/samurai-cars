50172f80263bcab6fdd26bb962bd3b1b923e385c
wesley <wesleybruno910@gmail.com>
2023-10-28 16:25:47 -0300

    feat: add EnableJpaAuditing annotation
    this allows to track modifications in the entities

--- src/main/java/br/com/project/samuraicars/SamuraiCarsApplication.java
+++ src/main/java/br/com/project/samuraicars/SamuraiCarsApplication.java
@@ -2,10 +2,11 @@ package br.com.project.samuraicars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SamuraiCarsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SamuraiCarsApplication.class, args);
 	}
