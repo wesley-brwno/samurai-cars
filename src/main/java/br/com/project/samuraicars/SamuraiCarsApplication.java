18be30fa638a7eea9024cdd86ee7b8dfca2ec720
wesley <wesleybruno910@gmail.com>
2023-10-28 13:45:43 -0300

    first commit

---
+++ src/main/java/br/com/project/samuraicars/SamuraiCarsApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.samuraicars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamuraiCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamuraiCarsApplication.class, args);
+	}
+
+}
