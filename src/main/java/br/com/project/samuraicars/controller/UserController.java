6f1fb9b0cf59dcb65b9b9a79f4097e29505ba2a5
wesley <wesleybruno910@gmail.com>
2023-10-28 17:24:34 -0300

    feat: add list method to return a UserResponseBody
    this method uses userService to find a user by id and return a UserResponseBody object, case it's not found an exception is thrown and the 400 Bad Request is returned as the response

--- src/main/java/br/com/project/samuraicars/controller/UserController.java
+++ src/main/java/br/com/project/samuraicars/controller/UserController.java
@@ -6,10 +6,10 @@ import br.com.project.samuraicars.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/users")
@@ -17,7 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
     @PostMapping
-    public ResponseEntity<UserResponseBody> save(@Valid @RequestBody UserPostRequestBody user) {
-        return ResponseEntity.ok().body(userService.save(user));
+    public ResponseEntity<UserResponseBody> save(@Valid @RequestBody UserPostRequestBody user, UriComponentsBuilder uriBuilder) {
+        UserResponseBody savedUser = userService.save(user);
+        URI uri = uriBuilder.path("/users/{id}").buildAndExpand(savedUser.id()).toUri();
+        return ResponseEntity.created(uri).body(savedUser);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseBody> list(@PathVariable Long id) {
+        return ResponseEntity.ok().body(userService.list(id));
     }
 }
