6c5e61f6224c4ede9818eb937d5dc4b601fcc269
wesley <wesleybruno910@gmail.com>
2023-10-28 17:20:29 -0300

    refactor: add JsonProperty to display property name in snake notation

--- src/main/java/br/com/project/samuraicars/DTO/user/UserResponseBody.java
+++ src/main/java/br/com/project/samuraicars/DTO/user/UserResponseBody.java
@@ -1,13 +1,18 @@
 package br.com.project.samuraicars.DTO.user;
 
 import br.com.project.samuraicars.model.User;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDateTime;
 
 public record UserResponseBody(
         Long id,
         String name,
-        String email
+        String email,
+        @JsonProperty("created_at")
+        LocalDateTime createdAt
 ) {
     public UserResponseBody(User user) {
-        this(user.getId(), user.getName(), user.getEmail());
+        this(user.getId(), user.getName(), user.getEmail(), user.getCreatedAt());
     }
 }
