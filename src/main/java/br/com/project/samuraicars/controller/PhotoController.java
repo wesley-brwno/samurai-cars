0d5d251109676d4b434c91e7b5dfa813ca681d56
wesley <wesleybruno910@gmail.com>
2023-11-10 10:28:20 -0300

    feat: working with photo upload

--- src/main/java/br/com/project/samuraicars/controller/PhotoController.java
+++ src/main/java/br/com/project/samuraicars/controller/PhotoController.java
@@ -1,9 +1,14 @@
 package br.com.project.samuraicars.controller;
 
 import br.com.project.samuraicars.service.VehiclePhotoService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -16,10 +21,13 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PhotoController {
     private final VehiclePhotoService vehiclePhotoService;
+
     @PostMapping
-    public ResponseEntity<?> save(@RequestParam List<MultipartFile> photos, @RequestParam Long vehicleId,
-                                  UriComponentsBuilder uriBuilder) {
-        vehiclePhotoService.save(photos, vehicleId);
+    public ResponseEntity<?> save(@RequestParam List<MultipartFile> photos,
+                                  @RequestParam Long vehicleId,
+                                  UriComponentsBuilder uriBuilder,
+                                  @AuthenticationPrincipal UserDetails userDetails) {
+        vehiclePhotoService.save(photos, vehicleId, userDetails);
         URI uri = uriBuilder.path("/photos/{vehicle_id}").buildAndExpand(vehicleId).toUri();
         return ResponseEntity.created(uri).build();
     }
