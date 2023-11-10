799641275264b5375869142ab8ff81d9bf16292d
wesley <wesleybruno910@gmail.com>
2023-11-10 10:27:57 -0300

    delete: removing non used files

--- src/main/java/br/com/project/samuraicars/repositoy/VehicleRespository.java
+++
@@ -1,7 +0,0 @@
-package br.com.project.samuraicars.repositoy;
-
-import br.com.project.samuraicars.model.Vehicle;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VehicleRespository extends JpaRepository<Vehicle, Long> {
-}
