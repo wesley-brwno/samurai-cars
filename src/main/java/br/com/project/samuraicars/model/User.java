8f074d4456d1718e264126ca40aadb2ea80d7f16
wesley <wesleybruno910@gmail.com>
2023-11-10 10:25:49 -0300

    feat: add authorities String attribute to User Entity and some methods to related classes

--- src/main/java/br/com/project/samuraicars/model/User.java
+++ src/main/java/br/com/project/samuraicars/model/User.java
@@ -1,33 +1,69 @@
 package br.com.project.samuraicars.model;
 
-import br.com.project.samuraicars.DTO.user.UserPostRequestBody;
+import br.com.project.samuraicars.DTO.authentication.UserRegisterPostRequestBody;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
-@Table(name = "system_users")
+@Table(name = "_users")
 @Getter
 @Setter
 @NoArgsConstructor
-public class User extends BaseEntity {
+@EqualsAndHashCode(of = "id", callSuper = true)
+public class User extends BaseEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private String email;
     private String password;
+    private String authorities;
     @OneToMany(mappedBy = "user")
     private List<Vehicle> vehicles;
 
-    public User(UserPostRequestBody data) {
-        this.name = data.name();
-        this.email = data.email();
-        this.password = data.password();
+    public User(String encode, UserRegisterPostRequestBody user) {
+        this.name = user.name();
+        this.email = user.email();
+        this.password = encode;
+        this.authorities = "USER";
     }
 
-//    TODO: implement interface UserDetails to configure Authorization and Authentication
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.stream(authorities.split(","))
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/br/com/project/samuraicars/repositoy/UserRepository.java
+++ src/main/java/br/com/project/samuraicars/repositoy/UserRepository.java
@@ -2,6 +2,12 @@ package br.com.project.samuraicars.repositoy;
 
 import br.com.project.samuraicars.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<UserDetails> findByEmail(String email);
+
 }
