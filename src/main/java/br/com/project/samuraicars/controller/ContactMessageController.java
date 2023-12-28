ee3d633d216a82f7eefc005fecccfbcd07bbd3e5
wesley <wesleybruno910@gmail.com>
2023-12-28 08:54:16 -0300

    Feat: Adds ContactMessageController to enable management of ContactMessage entities
    Enables CRUD operations through the following methods
    
    * save - persist an entity to database,
    * displayById - retrieves a specific message,
    * displayByUser - retrieves messages with their associated user
    * delete - removes a message by its id.

---
+++ src/main/java/br/com/project/samuraicars/controller/ContactMessageController.java
@@ -0,0 +1,51 @@
+package br.com.project.samuraicars.controller;
+
+import br.com.project.samuraicars.DTO.contactMessage.ContactMessageRequestBody;
+import br.com.project.samuraicars.DTO.contactMessage.ContactMessageResponseBody;
+import br.com.project.samuraicars.model.ContactMessage;
+import br.com.project.samuraicars.service.ContactMessageService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/messages")
+public class ContactMessageController {
+    private final ContactMessageService contactMessageService;
+
+    @PostMapping
+    public ResponseEntity<Void> save(@Valid @RequestBody ContactMessageRequestBody messageBody, UriComponentsBuilder uriBuilder) {
+        ContactMessage savedMessage = contactMessageService.save(messageBody);
+        URI uri = uriBuilder.path("/messages/{id}").buildAndExpand(savedMessage.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ContactMessageResponseBody> displayById(@PathVariable Long id, @AuthenticationPrincipal UserDetails user) {
+        return ResponseEntity.ok().body(contactMessageService.getById(id, user));
+    }
+
+    @GetMapping()
+    public ResponseEntity<Page<ContactMessageResponseBody>> displayByUser(@PageableDefault(sort = {"createdAt"}, direction = Sort.Direction.DESC) Pageable pageable,
+                                                                          @AuthenticationPrincipal UserDetails user) {
+        return ResponseEntity.ok().body(contactMessageService.findByUserPageable(pageable, user));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id, @AuthenticationPrincipal UserDetails user) {
+        contactMessageService.delete(id, user);
+        return ResponseEntity.noContent().build();
+    }
+}
