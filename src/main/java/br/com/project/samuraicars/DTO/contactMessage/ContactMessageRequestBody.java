fdbf1900716756112fd8881872914074202ddd70
wesley <wesleybruno910@gmail.com>
2023-12-28 08:38:16 -0300

    Feat: Adds DTOs for ContactMessage Entity
    Adds ContactMessageRequestBody and ContactMessageResponseBody. These classes allow communication without exposing sensitive data. The request body and response body are now more structured and easier to understand.

---
+++ src/main/java/br/com/project/samuraicars/DTO/contactMessage/ContactMessageRequestBody.java
@@ -0,0 +1,24 @@
+package br.com.project.samuraicars.DTO.contactMessage;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.*;
+
+public record ContactMessageRequestBody(
+        @NotBlank()
+        @Size(min = 2, max = 20)
+        String name,
+        @NotBlank()
+        @Size(min = 2, max = 20)
+        String lastname,
+        @Pattern(regexp = "\\d{10}", message = "Invalid phone number")
+        String phone,
+        @Email()
+        String email,
+        @NotBlank()
+        @Size(min = 10, max = 500)
+        String message,
+        @JsonProperty("vehicle_id")
+        @NotNull()
+        Long vehicleId
+) {
+}
