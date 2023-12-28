529a47b5d5e4d1d09a780212f71c39c0d45219fc
wesley <wesleybruno910@gmail.com>
2023-12-28 11:26:32 -0300

    Delete: Removes AuthenticationException, since it wasn't being used.

--- src/main/java/br/com/project/samuraicars/exception/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package br.com.project.samuraicars.exception;
-
-public class AuthenticationException extends RuntimeException {
-    public AuthenticationException(String message) {
-        super(message);
-    }
-}
