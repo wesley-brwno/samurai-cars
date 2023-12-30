f7c2af7a0902d940f6814c35bbbda31dda9836cb
wesley <wesleybruno910@gmail.com>
2023-12-30 16:28:35 -0300

    Feat: add validation annotations in attributes

--- src/main/java/br/com/project/samuraicars/DTO/vehicle/VehiclePutRequestBody.java
+++ src/main/java/br/com/project/samuraicars/DTO/vehicle/VehiclePutRequestBody.java
@@ -1,9 +1,20 @@
 package br.com.project.samuraicars.DTO.vehicle;
 
+import jakarta.validation.constraints.*;
+
 public record VehiclePutRequestBody(
+        @NotNull()
+        @Positive()
         Long id,
+        @NotBlank()
+        @Size(min = 2, max = 100)
         String name,
+        @NotBlank()
+        @Size(min = 2, max = 100)
         String model,
+        @NotNull()
+        @Digits(integer = 4, fraction = 0)
+        @Positive()
         Long year
 ) {
 }
