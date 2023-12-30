e941343ca64f5713cc323b586e206820b64bd9ad
wesley <wesleybruno910@gmail.com>
2023-12-30 16:25:58 -0300

    Refactor: fix type in class name and add new URL to allowedOrigins

--- src/main/java/br/com/project/samuraicars/configuration/security/WebConfig.java
+++ src/main/java/br/com/project/samuraicars/configuration/security/WebConfig.java
@@ -8,14 +8,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WeConfig {
+public class WebConfig {
     @Bean
     public WebMvcConfigurer cors() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("http://localhost:4200", "https://samurai-cars.vercel.app/")
                         .allowedHeaders(
                                 HttpHeaders.CONTENT_TYPE,
                                 HttpHeaders.AUTHORIZATION,
