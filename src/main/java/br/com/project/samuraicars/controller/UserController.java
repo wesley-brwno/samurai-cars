6f3ab1b58dde7ee9c16d5dfd0c0d9f3b40640959
Wesley Bruno <wesleybruno910@gmail.com>
2024-02-03 13:38:49 -0300

    refactor: removes UserDetailsRequestBody DTO getUserByAuthorizationHeader endpoint
    The data the DTO and the endpoint provides is no longer needed because this data is delivered via JWT Token.

--- src/main/java/br/com/project/samuraicars/controller/UserController.java
+++ src/main/java/br/com/project/samuraicars/controller/UserController.java
@@ -1,15 +1,10 @@
 package br.com.project.samuraicars.controller;
 
-import br.com.project.samuraicars.DTO.user.UserDetailsRequestBody;
 import br.com.project.samuraicars.DTO.user.UserPublicDetailsRequestBody;
 import br.com.project.samuraicars.model.User;
 import br.com.project.samuraicars.service.UserService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,13 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
-    @Operation(security = { @SecurityRequirement(name = "bearer-key") })
-    @GetMapping()
-    public ResponseEntity<UserDetailsRequestBody> getUserByAuthorizationHeader(
-            @AuthenticationPrincipal UserDetails userDetails) {
-        User user = userService.findUserByEmail(userDetails.getUsername());
-        return ResponseEntity.ok(new UserDetailsRequestBody(user.getId(), user.getName(), user.getAuthorities().toString()));
-    }
 
     @GetMapping("/{user_id}")
     public ResponseEntity<?> getUserById(@PathVariable("user_id") Long userId){
