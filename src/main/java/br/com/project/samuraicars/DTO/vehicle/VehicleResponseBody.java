87dc78fbb848a90d0b19735162f3034224c63177
Wesley Bruno <wesleybruno910@gmail.com>
2024-03-15 18:46:06 -0300

    refactor: removes personalized constructors and add @builder annotation

--- src/main/java/br/com/project/samuraicars/DTO/vehicle/VehicleResponseBody.java
+++ src/main/java/br/com/project/samuraicars/DTO/vehicle/VehicleResponseBody.java
@@ -3,9 +3,11 @@ package br.com.project.samuraicars.DTO.vehicle;
 
 import br.com.project.samuraicars.model.Vehicle;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 
 import java.time.LocalDateTime;
 
+@Builder
 public record VehicleResponseBody(
         @JsonProperty("created_at")
         LocalDateTime createdAt,
@@ -20,25 +22,4 @@ public record VehicleResponseBody(
         String brand,
         Double price
 ) {
-        public VehicleResponseBody(@JsonProperty("created_at")
-                                      LocalDateTime createdAt, Long id, String name, String model, Long year, @JsonProperty("user_id")
-                                      Long userId, @JsonProperty("vehicle_type")
-                                      String vehicleType, String brand, Double price) {
-                this.createdAt = createdAt;
-                this.id = id;
-                this.name = name;
-                this.model = model;
-                this.year = year;
-                this.userId = userId;
-                this.vehicleType = vehicleType;
-                this.brand = brand;
-                this.price = price;
-        }
-
-        public VehicleResponseBody(Vehicle vehicle) {
-                this(vehicle.getCreatedAt(), vehicle.getId(), vehicle.getName(), vehicle.getModel(), vehicle.getYear(),
-                        vehicle.getUser().getId(), vehicle.getVehicleType(), vehicle.getBrand(), vehicle.getPrice());
-        }
-
-
 }
