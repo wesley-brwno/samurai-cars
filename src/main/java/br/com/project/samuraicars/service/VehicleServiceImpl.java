70e344b6fbd6914a10d1988cbcff84c19fe488ed
Wesley Bruno <wesleybruno910@gmail.com>
2024-03-15 13:41:09 -0300

    feat: this class implements the interface VehicleService

---
+++ src/main/java/br/com/project/samuraicars/service/VehicleServiceImpl.java
@@ -0,0 +1,126 @@
+package br.com.project.samuraicars.service;
+
+import br.com.project.samuraicars.DTO.vehicle.VehicleWithPhotosResponseBody;
+import br.com.project.samuraicars.DTO.vehicle.VehiclePostRequestBody;
+import br.com.project.samuraicars.DTO.vehicle.VehiclePutRequestBody;
+import br.com.project.samuraicars.DTO.vehicle.VehicleResponseBody;
+import br.com.project.samuraicars.exception.BadRequestException;
+import br.com.project.samuraicars.model.User;
+import br.com.project.samuraicars.model.Vehicle;
+import br.com.project.samuraicars.model.VehiclePhoto;
+import br.com.project.samuraicars.repositoy.VehicleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class VehicleServiceImpl implements VehicleService {
+    private final VehicleRepository vehicleRepository;
+    private final UserService userService;
+
+    @Override
+    @Transactional
+    public VehicleResponseBody save(VehiclePostRequestBody vehiclePostRequestBody, UserDetails userDetails) {
+        Vehicle vehicle = vehicleRepository.save(mapVehiclePostRequestBodyToEntity(vehiclePostRequestBody, (User) userDetails));
+        return mapEntityToVehicleResponseBody(vehicle);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long vehicleId, UserDetails userDetails) {
+        Vehicle vehicle = findById(vehicleId);
+        if (userService.isUserOwnerOfResource(userDetails, vehicle) || userService.isUserAdmin(userDetails)) {
+            vehicleRepository.delete(vehicle);
+        }
+        throw new BadRequestException("The user is not authorized to perform this operation check their permissions.");
+    }
+
+    @Override
+    @Transactional
+    public VehicleResponseBody replace(VehiclePutRequestBody vehiclePutRequestBody, UserDetails userDetails) {
+        Vehicle vehicle = findById(vehiclePutRequestBody.id());
+        if (userService.isUserOwnerOfResource(userDetails, vehicle) || userService.isUserAdmin(userDetails)) {
+            Vehicle vehicleUpdated = vehicleRepository.save(mapVehiclePutRequestBodyToEntity(vehiclePutRequestBody, vehicle));
+            return mapEntityToVehicleResponseBody(vehicleUpdated);
+        }
+        throw new BadRequestException("The user is not authorized to perform this operation check their permissions.");
+    }
+
+    @Override
+    public VehicleWithPhotosResponseBody listById(Long vehicleId, UriComponentsBuilder uriComponentsBuilder) {
+        Vehicle vehicle = findById(vehicleId);
+        return createVehicleWithPhotosResponseBody(vehicle, uriComponentsBuilder);
+    }
+
+    @Override
+    public Page<VehicleWithPhotosResponseBody> listAll(Pageable pageable, UriComponentsBuilder uriComponentsBuilder) {
+        Page<Vehicle> vehiclePage = vehicleRepository.findAll(pageable);
+        return vehiclePage.map(vehicle -> createVehicleWithPhotosResponseBody(vehicle, uriComponentsBuilder));
+    }
+
+    @Override
+    public List<VehicleWithPhotosResponseBody> listByUser(User user, UriComponentsBuilder uriComponentsBuilder) {
+        List<Vehicle> vehicles = vehicleRepository.findAllByUser(user);
+        return vehicles.stream().map(vehicle -> createVehicleWithPhotosResponseBody(vehicle, uriComponentsBuilder)).toList();
+    }
+
+    private Vehicle findById(Long id) {
+        return vehicleRepository.findById(id).orElseThrow(() -> new BadRequestException("Vehicle not found!"));
+    }
+
+    private VehicleResponseBody mapEntityToVehicleResponseBody(Vehicle vehicle) {
+        return new VehicleResponseBody(
+                vehicle.getCreatedAt(),
+                vehicle.getId(),
+                vehicle.getVehicleType(),
+                vehicle.getModel(),
+                vehicle.getYear(),
+                vehicle.getUser().getId(),
+                vehicle.getVehicleType(),
+                vehicle.getBrand(),
+                vehicle.getPrice()
+        );
+    }
+
+    private Vehicle mapVehiclePostRequestBodyToEntity(VehiclePostRequestBody vehicleRequest, User user) {
+        return Vehicle.builder()
+                .name(vehicleRequest.name())
+                .model(vehicleRequest.model())
+                .year(vehicleRequest.year())
+                .vehicleType(vehicleRequest.vehicleType())
+                .brand(vehicleRequest.brand())
+                .price(vehicleRequest.price())
+                .user(user)
+                .build();
+    }
+
+    private Vehicle mapVehiclePutRequestBodyToEntity(VehiclePutRequestBody vehicleRequest, Vehicle vehicle) {
+        vehicle.setName(vehicleRequest.name());
+        vehicle.setModel(vehicleRequest.model());
+        vehicle.setYear(vehicleRequest.year());
+        vehicle.setVehicleType(vehicleRequest.vehicleType());
+        vehicle.setBrand(vehicleRequest.brand());
+        vehicle.setPrice(vehicleRequest.price());
+        return vehicle;
+    }
+
+    private VehicleWithPhotosResponseBody createVehicleWithPhotosResponseBody(Vehicle vehicle, UriComponentsBuilder uriBuilder) {
+        List<String> photoUrls = getPhotoUrls(getPhotoIds(vehicle.getPhotos()), uriBuilder);
+        return new VehicleWithPhotosResponseBody(mapEntityToVehicleResponseBody(vehicle), photoUrls);
+    }
+
+    private List<Long> getPhotoIds(List<VehiclePhoto> photos) {
+        return photos.stream().map(VehiclePhoto::getId).toList();
+    }
+    private List<String> getPhotoUrls(List<Long> photoIds, UriComponentsBuilder uriComponentsBuilder) {
+        String uriString = uriComponentsBuilder.toUriString();
+        return photoIds.stream().map(id -> uriString + "/photos/" + id).toList();
+    }
+}
