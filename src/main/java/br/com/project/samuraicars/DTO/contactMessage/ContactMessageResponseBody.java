214c89b39fa6a45303b5e6191f1d134310187017
Wesley Bruno <wesleybruno910@gmail.com>
2024-05-07 23:09:29 -0300

    Refactor: Removes non-used constructors

--- src/main/java/br/com/project/samuraicars/DTO/contactMessage/ContactMessageResponseBody.java
+++ src/main/java/br/com/project/samuraicars/DTO/contactMessage/ContactMessageResponseBody.java
@@ -1,6 +1,5 @@
 package br.com.project.samuraicars.DTO.contactMessage;
 
-import br.com.project.samuraicars.model.ContactMessage;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.LocalDateTime;
@@ -19,32 +18,4 @@ public record ContactMessageResponseBody(
         @JsonProperty("is_read")
         boolean isRead
 ) {
-        public ContactMessageResponseBody(String name, String lastName, String phone, String email, String message, Long id, @JsonProperty("vehicle_id")
-        Long vehicleId, @JsonProperty("created_at")
-                                          LocalDateTime createdAt, @JsonProperty("is_read")
-                                          boolean isRead) {
-                this.name = name;
-                this.lastName = lastName;
-                this.phone = phone;
-                this.email = email;
-                this.message = message;
-                this.id = id;
-                this.vehicleId = vehicleId;
-                this.createdAt = createdAt;
-                this.isRead = isRead;
-        }
-
-        public ContactMessageResponseBody(ContactMessage message) {
-                this(
-                        message.getName(),
-                        message.getLastname(),
-                        message.getPhone(),
-                        message.getEmail(),
-                        message.getMessage(),
-                        message.getId(),
-                        message.getVehicleId(),
-                        message.getCreatedAt(),
-                        message.isRead()
-                );
-        }
 }
