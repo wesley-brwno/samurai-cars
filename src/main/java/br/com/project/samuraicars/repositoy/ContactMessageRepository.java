00bd1d90572354e345f9b05250a7e8a7b4be527a
Wesley Bruno <wesleybruno910@gmail.com>
2024-05-07 23:06:09 -0300

    feat: add method findByIdAndUser(id, user);
    This method search for a message by its id and user and return an Optional object

--- src/main/java/br/com/project/samuraicars/repositoy/ContactMessageRepository.java
+++ src/main/java/br/com/project/samuraicars/repositoy/ContactMessageRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ContactMessageRepository extends JpaRepository<ContactMessage, Long> {
     Page<ContactMessage> findAllByUser(User user, Pageable pageable);
+    Optional<ContactMessage> findByIdAndUser(Long id, User user);
 }
